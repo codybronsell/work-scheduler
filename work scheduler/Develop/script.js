2631f2da3d8c18840f296768357238e2444cf17e
codybronsell <110582217+codybronsell@users.noreply.github.com>
2022-09-15 23:48:02 -0500

    fixed save function to work after refresh on all rows

--- work scheduler/Develop/script.js
+++ work scheduler/Develop/script.js
@@ -4,5 +4,45 @@ var mm = String(today.getMonth() + 1).padStart(2, '0');
 var yyyy = today.getFullYear();
 
 today = mm + '/' + dd + '/' + yyyy;
-document.querySelector(".today").innerHTML  = today
+document.querySelector(".today").innerHTML = today
 
+function text() {
+   var listitem = document.getElementById("listitem").value;
+
+   var listitemtext = localStorage.setItem("listitem", listitem)
+
+
+   var listitemtext = localStorage.getItem("listitem", listitem)
+
+
+}
+
+$(document).ready(function () {
+   var today = new Date();
+   var dd = String(today.getDate()).padStart(2, '0');
+   var mm = String(today.getMonth() + 1).padStart(2, '0');
+   var yyyy = today.getFullYear();
+
+   $('.row-time').each(function () {
+      var $this = $(this);
+      var $btn = $this.find('button');
+      var $desc = $this.find('input.form-control');
+       var time = $this.attr('data-time');
+      console.info(time);
+      var savedTime = localStorage.getItem(time);
+      if (savedTime) {
+         $desc.val(savedTime);
+      }
+
+      $btn.on('click', function () {
+         var val = $desc.val();
+         console.info(val);
+         console.info(time);
+         localStorage.setItem(time, val);
+
+      });
+
+   });
+
+
+});
